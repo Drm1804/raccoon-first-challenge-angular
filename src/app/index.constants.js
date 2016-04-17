e80d9135f260b4dd5a00aa1c7ab85dd0598a7eb6
Drm-a <drm-a@yandex.ru>
2016-04-17 16:57:29 +0500

    first commit

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('raccoon')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
